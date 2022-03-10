c3e3d95adc75d2db65cd725de5ecfb2d1d8093f4
Mariana <koval.nkzps@gmail.com>
2022-03-10 15:33:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+alert('Hello');
+
+const answers = [];
+answers[0] = prompt('Name', '');
+answers[1] = prompt('Surname', '');
+answers[2] = prompt('Age', '');
+
+document.write(answers);
